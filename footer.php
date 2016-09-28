81e79164ba4335e8ad6783a2c3344adcf1e42b30
Wes Reed <wesley.a.reed@gmail.com>
2016-09-28 17:09:52 -0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div class="sidebar">
+  <?php if(is_active_sidebar('sidebar')) : ?>
+    <?php dynamic_sidebar('sidebar'); ?>
+  <?php endif ?>
+</div>
+
+<div class="clear"></div>
+</div>
+
+<footer>
+  <div class="container">
+    <p>
+      &copy; <?php the_date('Y'); ?> - <?php bloginfo('name'); ?>
+    </p>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+    <div class="container">
+    <div class="main">
+        <?php if(have_posts()) : ?>
+
+          <?php while(have_posts()) : the_post(); ?>
+            <article class="post">
+              <h3><?php the_title(); ?></h3>
+              <div class="meta">
+                Created By: <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
+              </div>
+              <?php if(has_post_thumbnail()) : ?>
+                <div class="post-thumbnail">
+                  <?php the_post_thumbnail(); ?>
+                </div>
+              <?php endif; ?>
+              <?php the_content(); ?>
+              <br>
+            </article>
+          <?php endwhile; ?>
+
+       <?php else :  ?>
+            <?php echo wpautop('Sorry, no posts were found!'); ?>
+       <?php endif; ?>
+
+       <?php comments_template(); ?>
+      </div>
+
+<?php get_footer(); ?>
